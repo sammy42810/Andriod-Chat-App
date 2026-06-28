5066dd75cab9102c8b3dede86d1cabad2a17cdb5
Samantha Bryan <samanthabryan878@gmail.com>
2026-06-28 17:13:31 -0400

    Initial Build

---
+++ Application Architecture II/Assignment-Code/Chat-Server-Oneway/app/src/main/java/edu/stevens/cs522/chatserver/activities/ViewPeersActivity.java
@@ -0,0 +1,80 @@
+package edu.stevens.cs522.chatserver.activities;
+
+import android.content.Intent;
+import android.os.Build;
+import android.os.Bundle;
+import android.view.View;
+
+import androidx.activity.EdgeToEdge;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+import androidx.fragment.app.FragmentActivity;
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.ViewModelProvider;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.List;
+
+import edu.stevens.cs522.chatserver.R;
+import edu.stevens.cs522.chatserver.entities.Peer;
+import edu.stevens.cs522.chatserver.ui.TextAdapter;
+import edu.stevens.cs522.chatserver.viewmodels.PeersViewModel;
+
+
+public class ViewPeersActivity extends FragmentActivity implements TextAdapter.OnItemClickListener {
+
+    /*
+     * TODO See ChatServer for example of what to do, query peers database instead of messages database.
+     */
+
+    private TextAdapter<Peer> peerAdapter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.view_peers);
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.view_peers), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        // Initialize the recyclerview and adapter for peers
+        RecyclerView peersList = findViewById(R.id.peer_list);
+        peersList.setLayoutManager(new LinearLayoutManager(this));
+
+        peerAdapter = new TextAdapter<>(peersList, this);
+        peersList.setAdapter(peerAdapter);
+
+        // TODO create the view model and query for a list of all peers
+
+
+        // TODO observer for list of peers updates the peer adapter
+
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+    }
+
+    /*
+     * Callback interface defined in TextAdapter, for responding to clicks on rows.
+     */
+    @Override
+    public void onItemClick(RecyclerView parent, View view, int position) {
+        /*
+         * Clicking on a peer brings up details
+         */
+        Peer peer = peerAdapter.getItem(position);
+
+        Intent intent = new Intent(this, ViewPeerActivity.class);
+        intent.putExtra(ViewPeerActivity.PEER_KEY, peer);
+        startActivity(intent);
+
+    }
+}
---
+++ Application Architecture II/Assignment-Code/Chat-Server-Oneway/app/src/main/java/edu/stevens/cs522/chatserver/databases/MessageDao.java
@@ -0,0 +1,35 @@
+package edu.stevens.cs522.chatserver.databases;
+
+import androidx.lifecycle.LiveData;
+import androidx.room.Dao;
+import androidx.room.Insert;
+import androidx.room.Query;
+
+import java.util.List;
+
+import edu.stevens.cs522.chatserver.entities.Message;
+
+// TODO add annotations for Repository pattern
+public interface MessageDao {
+
+    /**
+     * Get all messages in the database.
+     * @return
+     */
+    public LiveData<List<Message>> fetchAllMessages();
+
+    /**
+     * Get all the messages for a particular peer.
+     * @param peerName
+     * @return
+     */
+    public LiveData<List<Message>> fetchMessagesFromPeer(String peerName);
+
+    /**
+     * Add a new message to the database.
+     * @param message
+     */
+    @Insert
+    public void persist(Message message);
+
+}
